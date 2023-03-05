398e349bb40f2f1d8d4e0083790e99fc5e1be197
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-03-05 17:10:26 -0300

    aula 04 php

---
+++ aula04/tiposdevariaveis.php
@@ -0,0 +1,14 @@
+<!dociType html>
+<html>
+<head>
+    <meta charset="UFT-8"/>
+    <title></title>
+</head>
+<body>
+    <?php 
+    $nome = " Rafael ";
+    $idade = 36;
+    echo $nome." tem ".$idade. " anos "
+    ?>
+</body>
+</html>
